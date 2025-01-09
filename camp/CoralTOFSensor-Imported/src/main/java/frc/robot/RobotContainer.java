677ac3f0ae218aa3e14ea0469a791d26b2d42db7
4682cybears <4682cybears@gmail.com>
2025-01-08 19:03:31 -0800

    changes

--- camp/CoralTOFSensor-Imported/src/main/java/frc/robot/RobotContainer.java
+++ camp/CoralTOFSensor-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -8,9 +8,11 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ImplementationTOF;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.ImplementationTOF;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -30,6 +32,7 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    ImplementationTOF implement = new ImplementationTOF();
   }
 
   /**
