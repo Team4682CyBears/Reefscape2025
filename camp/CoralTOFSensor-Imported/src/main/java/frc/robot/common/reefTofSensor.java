31a67e49e6dad1299de90a1d0a2317f7f281d754
jhall-07 <corduroydragon@gmail.com>
2025-01-08 18:18:40 -0800

    TOF sensor testing code

---
+++ camp/CoralTOFSensor-Imported/src/main/java/frc/robot/common/reefTofSensor.java
@@ -0,0 +1,62 @@
+package frc.robot.common;
+
+import com.playingwithfusion.TimeOfFlight;
+import com.playingwithfusion.TimeOfFlight.RangingMode;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class reefTofSensor {
+    private static final double noteDetectionThreshold = 8.0;
+    private TimeOfFlight tofSensor;
+    private int canID;
+    private String displayName;
+
+    public reefTofSensor(int canID){
+        tofSensor = new TimeOfFlight(canID);
+        this.canID = canID;
+        this.displayName = "TOF ID" + this.canID;
+
+        tofSensor.setRangingMode(RangingMode.Short, 20);
+        System.out.println("DONE CONFIG of TOF SENSOR at CanID" + canID);
+    }
+
+    public void blinkSensor(){
+        tofSensor.identifySensor();
+    }
+
+    public String getDisplayName(){
+        return displayName;
+    }
+
+    public double getRangeInches(){
+        return Units.metersToInches(tofSensor.getRange()/1000);
+    }
+
+    public final double getRangeSigme(){
+        return tofSensor.getRangeSigma();
+    }
+
+    public boolean isNoteDetected(){
+        double currentRangeInches = this.getRangeInches();
+        if(this.isRangeValid() && (currentRangeInches < noteDetectionThreshold)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isRangeValid(){
+        return tofSensor.isRangeValid();
+    }
+
+    public void publishTelemetery(){
+        SmartDashboard.putNumber(displayName + " Range Inches" , this.getRangeInches());
+        SmartDashboard.putBoolean(displayName + " Note Detected", this.isNoteDetected());
+        SmartDashboard.putBoolean(displayName + " Range Is Valid", this.isRangeValid());
+        SmartDashboard.putString(displayName + " TOF Status", this.tofSensor.getStatus().toString());
+      } 
+    
+}
+
+
+
