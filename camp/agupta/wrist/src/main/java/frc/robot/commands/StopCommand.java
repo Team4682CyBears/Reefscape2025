b7cf2cdf97d1e751b26bcc890d68230c413dc663
Abhay Gupta <ab18gu@gmail.com>
2025-02-19 15:29:06 -0800

    Copy over the climber code for reefscape

---
+++ camp/agupta/wrist/src/main/java/frc/robot/commands/StopCommand.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.TalonMotorSubsystem;
+
+
+public class StopCommand extends Command {
+    TalonMotorSubsystem motorSubsystem;
+
+    public StopCommand(TalonMotorSubsystem motorSubsystem) {
+        this.motorSubsystem = motorSubsystem;
+        
+        addRequirements(motorSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        motorSubsystem.motorStop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
