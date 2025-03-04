4e84ea359a34068310367244cad05f13ca2fea5a
asherbolotski <70773941+asherbolotski@users.noreply.github.com>
2025-03-04 14:46:07 -0800

    Update DefaultEndEffectorCommand.java

--- theRobot/src/main/java/frc/robot/commands/DefaultEndEffectorCommand.java
+++ theRobot/src/main/java/frc/robot/commands/DefaultEndEffectorCommand.java
@@ -3,7 +3,7 @@
 // Home of the Cybears
 // FRC - Reefscape - 2025
 // File: IntakeCoralCommand.java
-// Intent: Command to intake coral
+// Intent: Defualt command for the end effector
 // ************************************************************
 
 // ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
@@ -21,37 +21,24 @@ import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.EndEffectorSubsystem;
 
 public class DefaultEndEffectorCommand extends Command {
-    // The subsystem that controls the end effector mechanism
     private final SubsystemCollection subsystems;
-
-    private boolean seenPiece = false;
 
     /**
      * Creates a new IntakeCoralCommand.
      * 
-     * @param subsystem The EndEffectorSubsystem that this command will use
+     * @param subsystem The subsystem collection
      */
     public DefaultEndEffectorCommand(SubsystemCollection subsystems) {
         this.subsystems = subsystems;
         addRequirements(subsystems.getEndEffectorSubsystem());
     }
 
-    /**
-     * Called when the command is initially scheduled.
-     * Checks if a branch is already detected and sets initial state.
-     */
     @Override
     public void initialize() {
-        seenPiece = false;
     }
 
-    /**
-     * Called repeatedly when this Command is scheduled to run.
-     * Controls the end effector for coral handoff until a branch is detected.
-     */
     @Override
     public void execute() {
-        // Check for branch detection and stop if detected
         if(subsystems.getEndEffectorSubsystem().isCoralDetected() && subsystems.getElevatorHeightState().gElevatorPosition() == ElevatorPositions.STOW){
             subsystems.getEndEffectorSubsystem().setDirection(EndEffectorDirection.CORAL);
             subsystems.getEndEffectorSubsystem().setSpeed(EndEffectorSpeed.HANDOFF);
@@ -61,23 +48,8 @@ public class DefaultEndEffectorCommand extends Command {
         }
     }
 
-    /**
-     * Called once when the command ends or is interrupted.
-     * 
-     * @param interrupted whether the command was interrupted/canceled
-     */
     @Override
     public void end(boolean interrupted) {
         subsystems.getEndEffectorSubsystem().stop();
-    }
-
-    /**
-     * Returns true when the command should end.
-     * 
-     * @return true if a branch has been detected and handled
-     */
-    @Override
-    public boolean isFinished() {
-        return false;
     }
 }
