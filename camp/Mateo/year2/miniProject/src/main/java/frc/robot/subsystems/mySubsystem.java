92b79c7af386dc426098320190f1070a7a70b9e8
Alpha-Martial <mateo.herrman@gmail.com>
2025-09-23 17:55:05 -0700

    Setting up basic layout

---
+++ camp/Mateo/year2/miniProject/src/main/java/frc/robot/subsystems/mySubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class mySubsystem extends SubsystemBase{
+    public mySubsystem() {
+        
+    }
+}
